68d9c290c002565d74a8c2d689a9613832aef2c5
ahmedsayed2000 <ahmedsayedsaif76@gmail.com>
2023-07-03 16:42:56 +0300

    adding bucket code

--- GCloudASR/asr.py
+++ GCloudASR/asr.py
@@ -1,5 +1,15 @@
 from google.cloud import speech
+from google.cloud import storage
 import argparse
+
+storage_client = storage.Client.from_service_account_json('key.json')
+bucket_name = 'demo_audio_bucket_00'
+
+def upload_to_bucket(blob_name, file_path, bucket_name):
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    blob.upload_from_filename(file_path)
+    return blob
 
 
 # first read the audio file and its extension
@@ -8,6 +18,10 @@ parser.add_argument('--audio', type=str, required=True, help='The audio file to 
 parser.add_argument('--ext', type=str, required=True, help='The extension of the audio file.')
 args = parser.parse_args()
 
+upload_to_bucket(args.audio, args.audio, bucket_name)
+
+file_uri = 'gs://' + bucket_name + '/' + args.audio
+
 # load the audio
 file_name = args.audio
 extension = args.ext
@@ -15,12 +29,12 @@ file = file_name.split('.')[0]
 # initialize the client
 client = speech.SpeechClient.from_service_account_file('key.json')
 
-# load the audio
-with open(file_name, 'rb') as audio_file:
-    content = audio_file.read()
+# # load the audio
+# with open(file_name, 'rb') as audio_file:
+#     content = audio_file.read()
 
 # configure the audio settings
-audio = speech.RecognitionAudio(content=content)
+audio = speech.RecognitionAudio(uri=file_uri)
 
 # configure the speech settings based on the extension of the audio file
 # and enable word time offsets
@@ -38,13 +52,12 @@ elif extension == 'flac':
         enable_word_time_offsets=True)
 elif extension == 'mp3':
     config = speech.RecognitionConfig(
-        encoding=speech.RecognitionConfig.AudioEncoding.MP3,
         sample_rate_hertz=44100,
         language_code='en-US',
         enable_word_time_offsets=True)
 
 # transcribe the audio
-response = client.recognize(config=config, audio=audio)
+response = client.long_running_recognize(config=config, audio=audio)
 
 # extract the text from the response
 all_transcript = response.results[0].alternatives[0].transcript
